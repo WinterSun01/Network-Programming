64c1f19437f4bbc2056a3087c14fac8bd3e312bf
Анастасия Романова <nastya-danilyuk9@mail.ru>
2025-01-10 11:33:21 +0500

    Add project files.

---
+++ IPcalc/main.cpp
@@ -0,0 +1,29 @@
+#include<Windows.h>
+#include<CommCtrl.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_MAIN_DIALOG), NULL, (DLGPROC)DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HWND hUpDown = GetDlgItem(hwnd, IDC_SPIN_PREFIX);
+		SendMessage(hUpDown, UDM_SETRANGE, 0, MAKELPARAM(30, 1));
+	}
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
